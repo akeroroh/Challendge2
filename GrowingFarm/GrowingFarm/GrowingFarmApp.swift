ad303b59b4a2688a82570420ad67eb6b39650bcd
akeroroh <jinahyun@10-141-41-61.0101.pos.kr.iosda.org>
2025-04-16 14:53:07 +0900

    프로젝트 파일 생성

---
+++ GrowingFarm/GrowingFarm/GrowingFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowingFarmApp.swift
+//  GrowingFarm
+//
+//  Created by 진아현 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GrowingFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrowingFarm/GrowingFarmTests/GrowingFarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrowingFarmTests.swift
+//  GrowingFarmTests
+//
+//  Created by 진아현 on 4/16/25.
+//
+
+import Testing
+@testable import GrowingFarm
+
+struct GrowingFarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
